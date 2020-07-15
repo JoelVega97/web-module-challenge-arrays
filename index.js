6dfa89e19ad3c98afeed15cbb67c2d5202f52b3f
joel vega <joelvega97@yahoo.com>
2020-07-15 19:57:28 -0400

    finished task 6 and working on 7

--- index.js
+++ index.js
@@ -139,12 +139,12 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(newArr, arr){
+function copy(newArr,arr){
    newArr = [...arr]
-   if(newArr === arr){
        return newArr
    }
-}
+
+console.log(copy('newArr',originalFlavors))
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
@@ -161,13 +161,16 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
-
-    /*code here*/
-
+function filterByWord(arr, string){
+    let chocolateArr = []
+    for(let i = 0; i<arr.length; i++){
+        if(arr.includes(string)){
+            return chocolateArr
+        }
+    }
 }
 
-
+console.log(filterByWord(originalFlavors, 'Chocolate'))
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
 
